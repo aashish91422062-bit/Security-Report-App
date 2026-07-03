70a955363770ee92b7943d6398120dfa6e3b5360
aashish91422062-bit <aashish91422062@gmail.com>
2026-07-03 09:51:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function showMessage() {
-  alert("Welcome to AI Photo & Video Editing Website!");
+    alert("Welcome to AI Photo & Video Studio!");
+}
+
+function previewImage(event) {
+    const image = document.getElementById("preview");
+    image.src = URL.createObjectURL(event.target.files[0]);
+    image.style.display = "block";
 }
