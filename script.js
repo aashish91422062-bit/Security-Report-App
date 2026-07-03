5aa264e08ed868b85b9f450a7d7c86ca8560a1af
aashish91422062-bit <aashish91422062@gmail.com>
2026-07-03 11:18:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ function previewImage(event) {
     image.src = URL.createObjectURL(event.target.files[0]);
     image.style.display = "block";
 }
+function previewVideo(event) {
+    const video = document.getElementById("videoPreview");
+    video.src = URL.createObjectURL(event.target.files[0]);
+    video.style.display = "block";
+}
+
+function downloadMessage() {
+    alert("Download feature coming soon!");
+}
+
+function removeImage() {
+    document.getElementById("preview").src = "";
+    document.getElementById("preview").style.display = "none";
+}
+
+function removeVideo() {
+    document.getElementById("videoPreview").src = "";
+    document.getElementById("videoPreview").style.display = "none";
+}
